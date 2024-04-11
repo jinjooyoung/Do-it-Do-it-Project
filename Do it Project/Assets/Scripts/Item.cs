44762d5d38fb9bf66f945c74d32ec5171a15745d
jinjooyoung <jjy26297449@gmail.com>
2024-04-11 16:17:06 +0900

    이동, 아이템 획득, 카메라 무빙, 애니 코드 구현
    정윤호 코드 작성 복붙 + 약간 수정하여 푸시

---
+++ Do it Project/Assets/Scripts/Item.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+
+    public float rotateSpeed;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime, Space.World);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerscript player = other.GetComponent<playerscript>(); //Player의 스크립트 컴포넌트 가져오기
+            player.itemCount++; //아이템 카운트 + 1
+            print("아이탬이 들어왔습니다.");
+            gameObject.SetActive(false); //오브젝트 비활성화
+        }
+
+    }
+}
---
+++ Do it Project/Assets/Scripts/playerscript.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class playerscript : MonoBehaviour
+{
+    public float speed;
+    public int itemCount;
+
+    float hAxis;
+    float vAxis;
+/*
+    bool WDown;*/
+    //bool jump;
+
+
+    //bool isJump;
+
+    Vector3 moveVec;
+
+    Rigidbody rb; // 케릭터를 움직이기 위해 선언
+    Animator anim;
+
+    GameObject nearObject;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
+
+    }
+    void Update()
+    {
+        GetInput();
+        Move();
+        Turn();
+        //Jump();
+
+    }
+
+
+    void GetInput()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+    /*    WDown = Input.GetButton("wolk");*/
+        //jump = Input.GetButtonDown("Jump");
+
+
+
+    }
+    
+
+    void Move()
+    {
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        transform.position += moveVec * speed * Time.deltaTime;
+
+        anim.SetBool("isRun", moveVec != Vector3.zero);
+/*        anim.SetBool("isWolk", WDown);*/
+    }
+
+    void Turn()
+    {
+        transform.LookAt(transform.position + moveVec);
+    }
+
+    /*void Jump() // 점프
+    {
+        if (jump && !isJump) // ! 부정문 bool 값만 가능
+        {
+            rb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+            anim.SetBool("triggerJump", true);
+            isJump = true;
+        }
+    }*/
+
+
+   /* private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Plane")
+        {
+            anim.SetBool("triggerJump", false);
+            isJump = false;
+        }
+    }*/
+
+
+}
