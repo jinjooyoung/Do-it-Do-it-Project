479fccc593ad60314943c18c8071b149127cf20f
jinjooyoung <jjy26297449@gmail.com>
2024-04-18 16:58:33 +0900

    [Feat] 리소스 적용, 이동 테스트, 애니메이션 적용
    다음차시까지 Physics.OverlapSphere 을 이용하여 공격 범위 구현

---
+++ Do it Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 8f;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+
+        //실제 이동 속도를 입력값과 이동 속력을 사용해 결정
+        float xSpeed = xInput * speed;
+        float ySpeed = yInput * speed;
+
+        //Vector3 속도를 (xSpeed, 0, zSpeed)로 생성
+        Vector3 newVelocity = new Vector3(xSpeed, ySpeed, 0);
+        //리지드바디의 속도에 newVelocity 할당
+        rb.velocity = newVelocity;
+    }
+
+}
