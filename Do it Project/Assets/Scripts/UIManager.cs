fe44ca4f714d7c711966c131ada1cb87d318d9cc
Taehyeon88 <Taehyeon5376@naver.com>
2024-05-27 00:55:16 +0900

    [Feat]사운드파일, 오브젝트 추가배치

--- Do it Project/Assets/Scripts/UIManager.cs
+++ Do it Project/Assets/Scripts/UIManager.cs
@@ -71,8 +71,8 @@ public class UIManager : MonoBehaviour
         {
             credit.SetActive(true);
             isPlaying = true;
-            credit1.transform.position = Vector2.MoveTowards(credit1.transform.position, endPosition1.transform.position, 0.7f);
-            credit2.transform.position = Vector2.MoveTowards(credit2.transform.position, endPosition2.transform.position, 0.7f);
+            credit1.transform.position = Vector2.MoveTowards(credit1.transform.position, endPosition1.transform.position, 0.9f);
+            credit2.transform.position = Vector2.MoveTowards(credit2.transform.position, endPosition2.transform.position, 0.9f);
             if(isPlaying2 == true)
             {
                 credit.SetActive(false);
@@ -87,7 +87,7 @@ public class UIManager : MonoBehaviour
         {
             Timer2 += Time.deltaTime;
         }
-        if (Timer2 >= 48f)
+        if (Timer2 >= 49f)
         {
             isPlaying2 = true;
         }
